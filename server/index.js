acd391e808570376cfe503394aa5192823521e63
dhairyalakhmani <dhairyalakhmani27@gmail.com>
2026-09-02 16:51:39 +0530

    server setup completed

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import dns from 'node:dns';
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+if (dns.getServers().includes('127.0.0.1')) {
+    dns.setServers(['8.8.8.8', '1.1.1.1']);
+}
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send("Hello from the server");
+})
+
+const dbURL = process.env.MONGODB_URL;
+
+mongoose.connect(dbURL).then(() => {
+    console.log("DB connected")
+    app.listen(PORT, () => (
+        console.log("Server listening at port ", PORT)
+    ))
+}).catch((error) => {
+    console.log("Failed due to the error: ", error)
+})
